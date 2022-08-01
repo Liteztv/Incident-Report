55868739a26c4b7fd3aa69147de3b682528a873f
litztv <prlutz13@gmail.com>
2022-08-01 17:29:17 -0600

    redirect

--- Incident/Incident/urls.py
+++ Incident/Incident/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic import RedirectView
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Report/',include('Report.urls'))
+    path('Report/',include('Report.urls')),
+    path('',RedirectView.as_view(url='/Report/'))
 ]
