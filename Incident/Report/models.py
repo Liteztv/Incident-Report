33cb171a1eb06bc58ecc56054ad35883154ac8ec
TheJonry <110304328+TheJonry@users.noreply.github.com>
2022-08-01 21:48:10 -0400

    model test

--- Incident/Report/models.py
+++ Incident/Report/models.py
@@ -25,7 +25,7 @@ class FireReport(models.Model):
     date_and_time_of_incident_end = models.DateTimeField()
     incident_commander = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='FireCommander')
     incident_type = models.ForeignKey(IncidentType, on_delete=models.SET_NULL, null=True, help_text='IF OTHER PROVIDE DESCRIPTION')
-    cause_of_incident = models.TextField()
+    cause_of_incident = models.TextField(widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
     actions_taken = models.TextField()
     equipment_used = models.TextField()
     debrief_attendance = models.TextField()
