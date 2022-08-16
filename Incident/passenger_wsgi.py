1bbc2806a71cfe58f0e42adea088f7772c29b4ef
litztv <prlutz13@gmail.com>
2022-08-15 18:36:33 -0600

    1111111

---
+++ Incident/passenger_wsgi.py
@@ -0,0 +1,22 @@
+class PassengerPathInfoFix(object):
+    """
+    Sets PATH_INFO from REQUEST_URI since Passenger doesn't provide it.
+    """
+    def init(self, app):
+        self.app = app
+
+    def call(self, environ, start_response):
+        from urllib.parse import unquote
+        environ['SCRIPT_NAME'] = SCRIPT_NAME
+
+        request_uri = unquote(environ['REQUEST_URI'])
+        script_name = unquote(environ.get('SCRIPT_NAME', ''))
+        offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
+        environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
+        return self.app(environ, start_response)
+
+
+# Replace projectname with the name of the main folder containing wsgi.py and setting.py
+import Incident.wsgi
+application = Incident.wsgi.application
+application = PassengerPathInfoFix(application)
