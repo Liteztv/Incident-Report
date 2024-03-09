2fa7cfa27ae65adfc991add54d1409752ae8d94e
liteztv <prlutz13@gmail.com>
2024-03-09 16:41:26 -0500

    yes no model

--- Incident/Report/models.py
+++ Incident/Report/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from sympy import ordered
 
+class YesNo(models.Model):
+    description = models.CharField(max_length=140,blank=True, null=True)
+
+    def __str__(self):
+        return self.type
 
 class IncidentType(models.Model):
     type = models.CharField(max_length=100)
